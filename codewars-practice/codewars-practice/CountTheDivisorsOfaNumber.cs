6af5e3a68e819db1d6d7e169e4d4d507e0479773
jaka <jenalievas2003@stud.uni-obuda.hu>
2023-12-04 23:45:29 +0100

    count the divisors of a number

---
+++ codewars-practice/codewars-practice/CountTheDivisorsOfaNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace codewars_practice
+{
+	public class CountTheDivisorsOfaNumber
+	{
+		public CountTheDivisorsOfaNumber()
+		{
+		}
+	}
+}
+
---
+++ codewars-practice/codewars-practice/SquareDoubled.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace codewars_practice
+{
+	public class SquareDoubled
+	{
+        // Write a static function "Square" here which takes a double
+        // and returns the square of that double
+
+        //my solution
+        public static double Square(double number)
+        {
+            number *= number;
+            return number;
+        }
+    }
+}
+

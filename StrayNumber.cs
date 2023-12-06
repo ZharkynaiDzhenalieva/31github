9ad5ba771a66604c3c69f32be783dc3f89921560
ZHARKYNAI <142582233+ZharkynaiDzhenalieva@users.noreply.github.com>
2023-12-07 00:00:44 +0100

    stray numbers

---
+++ StrayNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace codewars_practice
+{
+	public class StrayNumber
+	{
+        //        You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+        //Complete the method which accepts such an array, and returns that single different number.
+
+        //The input array will always be valid! (odd-length >= 3)
+
+        //Examples
+        //[1, 1, 2] ==> 2
+        //[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+        public static int Stray(int[] numbers)
+        {
+            int number = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                number ^= numbers[i];
+            return number;
+
+        }
+    }
+}
+

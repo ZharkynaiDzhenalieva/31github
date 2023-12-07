5f281c578a0824b00864181dc9f0e1a84db8c430
ZHARKYNAI <142582233+ZharkynaiDzhenalieva@users.noreply.github.com>
2023-12-07 23:54:26 +0100

    Debugging sayHello function

---
+++ Debugging sayHello function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace codewars_practice
+{
+	public class Debugging_sayHello_function
+	{
+        //        The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard.It is your job to fix the code and get the program working again!
+        //Example output:
+        //Hello, Mr.Spock
+
+        //my solution
+        public class Kata
+        {
+            public static string SayHello(string name)
+            {
+                return $"Hello, {name}";
+            }
+        }
+    }
+}
+

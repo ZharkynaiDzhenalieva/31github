76dc6352ecd6a2ac418bdb8366364b542e8d6dc1
ZHARKYNAI <142582233+ZharkynaiDzhenalieva@users.noreply.github.com>
2023-12-08 23:02:57 +0100

    Beginner Series #2 Clock (codewars solution)

---
+++ Beginner Series #2 Clock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsTheStringUppercase
+{
+    /*Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000*/
+
+    //My solution
+    public static class Clock
+    {
+        public static int Past(int h, int m, int s)
+        {
+            //#Happy Coding! ^_^
+            int msHours = h * 3600000;
+            int msMinutes = m * 60000;
+            int msSeconds = s * 1000;
+
+            return msHours + msMinutes + msSeconds;
+
+
+        }
+    }
+}
